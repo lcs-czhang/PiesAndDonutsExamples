7a370d3ada4ec25bdf3ff1282814820d3efcfa51
Youzhou Zhang <czhang@lcs.on.ca>
2024-11-08 23:43:24 -0500

    addded pie view

--- PiesAndDonutsExamples/ContentView.swift
+++ PiesAndDonutsExamples/ContentView.swift
@@ -59,6 +59,36 @@ struct OneDimensionalView: View {
     }
 }
 
+struct PieView: View {
+    private var coffeeSales = [
+        (name: "Americano", count: 120),
+        (name: "Cappuccino", count: 234),
+        (name: "Espresso", count: 62),
+        (name: "Latte", count: 625),
+        (name: "Mocha", count: 320),
+        (name: "Affogato", count: 50)
+    ]
+    var body: some View {
+        Chart {
+            ForEach(coffeeSales, id: \.name) { coffee in
+
+                SectorMark(
+                    angle: .value("Cup", coffee.count),
+                    outerRadius: coffee.name == "Latte" ? 150 : 120,
+                    angularInset: 2.0
+                )
+                .foregroundStyle(by: .value("Type", coffee.name))
+                .annotation(position: .overlay) {
+                    Text("\(coffee.count)")
+                        .font(.headline)
+                        .foregroundStyle(.white)
+                }
+            }
+        }
+        .frame(height: 500)
+    }
+}
+
 #Preview {
     ContentView()
 }
